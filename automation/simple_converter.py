2e41c87b96afd1666c0d4156cba95531b2c03128
H. “Lyūke” Monaggem <hydracor@163.com>
2022-10-15 22:10:02 +0800

    ELT Archive Upload

---
+++ automation/simple_converter.py
@@ -0,0 +1,19 @@
+import re
+from resources import *
+
+class GehligMarkupProcessor:
+
+	_valid_options = {
+		"USE_TEMPLATE": "article_template",
+		"ARTICLE_LANG_DEFAULT": "zh",
+		"BACK_WAY": True,
+		"IGNORE_ARCHIVED": False
+	}
+
+	def __init__(se, raw:str, **options):
+		se.text = raw
+		for o, v in _valid_options.items():
+			se.__dict__[o] = options.get(o, v)
+
+	def segment_preprocess(se):
+		'''Not complete yet.'''
