0897002f8df2dd4bcd8d709269691c923b881f5f
H. “Lyūke” Monaggem <654383443@qq.com>
2026-03-31 14:52:45 +0800

    Update LHanC Romanization

--- scripts/nebty_ipa_applier.js
+++ scripts/nebty_ipa_applier.js
@@ -2,12 +2,16 @@ document.addEventListener(
 	"DOMContentLoaded",
 
 	function () {
-		var entries = document.getElementsByTagName("ipa")
-		var finalEntries = []
+		let entries = document.getElementsByTagName("ipa")
+		let finalEntries = []
 		for (ent = 0; ent < entries.length; ++ent) {
-			var thisEntry = entries[ent]
-			var converted = document.createElement("span")
+			let thisEntry = entries[ent]
+			let thisEntryClass = thisEntry.classList
+			let converted = document.createElement("span")
 			converted.classList.add("ipa")
+			if (thisEntryClass.length > 0) {
+				converted.classList.add(...thisEntryClass)
+			}
 			converted.innerText = nebty(thisEntry.innerText)[0]
 			finalEntries.push([converted, thisEntry])
 		}
